06fbbe454f0c5b4c0646202ad3b90b8034842a04
Josh Grant <jwaltgrant@gmail.com>
2020-06-04 21:35:18 -0500

    initial pass

--- ndb_drf/ndb_generics.py
+++ ndb_drf/ndb_generics.py
@@ -1,4 +1,91 @@
+from __future__ import annotations
+import typing
 from rest_framework.generics import GenericAPIView
+from django.http import Http404
 
 class NDBGenericAPIView(GenericAPIView):
-    pass
+    """
+    Generic API View that allows for use with Google NDB
+    Largest difference is that get query_set needs to return a 
+    ndb.Query object, rather than query results
+    
+    It is recomended to define the `model_class` property on the class
+    as that alows for using query filtering rather that looping over key results
+    for the implementation of `get_object`
+    """
+
+    lookup_field = 'key'
+    model_class = None
+
+    def get_queryset(self):
+        """
+        Get the list of items for this view.
+        This must be an iterable, and may be a queryset.
+        Defaults to using `self.queryset`.
+        This method should always be used rather than accessing `self.queryset`
+        directly, as `self.queryset` gets evaluated only once, and those results
+        are cached for all subsequent requests.
+        You may want to override this if you need to provide different
+        querysets depending on the incoming request.
+        (Eg. return a list of items that is specific to the user)
+        """
+        assert self.queryset is not None, (
+            "'%s' should either include a `queryset` attribute, "
+            "or override the `get_queryset()` method."
+            % self.__class__.__name__
+        )
+
+        return self.queryset
+
+    def _get_with_model(self, key: ndb.Key):
+        """
+        Get the Object by filtering with the model class
+        This is the optimal implementation to use
+        """
+        query_set = self.get_queryset()
+        query_set.filter(self.model_class.key == key)
+        return query_set.get()
+
+    def _get_with_loop(self, key: ndb.Key):
+        """
+        Fetch the keys of the results of the query set
+        Then loop over, attempting to find a match of the key,
+        return the object if key is found in query set results
+        """
+        for _key in self.get_queryset().fetch(keys_only=True):
+            if _key == key:
+                return _key.get()
+
+    def get_object(self) -> ndb.Model:
+        """
+        Get NDB Model intance from the query set
+        :raises: 404 if key is not in the query set
+        :return: ndb.Model
+        """
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (
+            'Expected view %s to be called with a URL keyword argument '
+            'named "%s". Fix your URL conf, or set the `.lookup_field` '
+            'attribute on the view correctly.' %
+            (self.__class__.__name__, lookup_url_kwarg)
+        )
+        lookup_key = self.kwargs.get(lookup_url_kwarg)
+        if isinstance(lookup_key, (bytes, str)):
+            lookup_key = ndb.Key(urlsafe=lookup_key)
+        
+        obj = None
+        
+        if self.model_class:
+            obj = self._get_with_model(lookup_key)
+        else:
+            obj = self._get_with_loop(lookup_key)
+        if not obj:
+            raise Http404
+        
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
+if typing.TYPE_CHECKING:
+    from google.cloud import ndb
---
+++ ndb_drf/ndb_mixins.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+import typing
+from rest_framework import mixins
+from rest_framework.response import 
+
+class NDBDestroyModelMixin(mixins.DestroyModelMixin):
+    def perform_destroy(self, instance: ndb.Model):
+        instance.key.delete()
+
+class ListModelMixin:
+    """
+    List a queryset.
+    """
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset()).fetch()
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+if typing.TYPE_CHECKING:
+    from google.cloud import ndb
