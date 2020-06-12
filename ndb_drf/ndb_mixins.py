c0a6a528d666e9d3a52fc6af383f8aac05f19e5a
Josh Grant <jwaltgrant@gmail.com>
2020-06-11 19:08:38 -0500

    start lazy query

--- ndb_drf/ndb_mixins.py
+++ ndb_drf/ndb_mixins.py
@@ -12,7 +12,7 @@ class NDBListModelMixin(mixins.ListModelMixin):
     List a queryset.
     """
     def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset()).fetch()
+        queryset = self.filter_queryset(self.get_queryset())
 
         page = self.paginate_queryset(queryset)
         if page is not None:
